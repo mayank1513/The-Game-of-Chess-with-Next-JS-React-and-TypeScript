cca66a8b80e6e6d6567b660cc85a84d24561852c
Mayank <mayank.srmu@gmail.com>
2022-09-28 15:32:17 +0530

    Highlight possible moves

--- components/board.tsx
+++ components/board.tsx
@@ -1,20 +1,15 @@
-import { Chess } from "chess.js";
 import { useEffect, useState } from "react";
 import styles from "./board.module.scss";
-import { p0, pb, pw } from "utils/chess-utils";
+import { p0, pb, pw, chess, getBoard } from "utils/chess-utils";
+import { Move } from "chess.js";
 
 export default function Board() {
   const [pieces, setPieces] = useState(
     new Array(8).fill(0).map(() => new Array(8).fill(""))
   );
+  const [highlighted, setHighlighted] = useState<string[]>([]);
   useEffect(() => {
-    const chess = new Chess();
-    const b = chess
-      .ascii()
-      .split("\n")
-      .slice(1, 9)
-      .map((rank) => rank.slice(5, 27).split("  "));
-    setPieces(b);
+    setPieces(getBoard());
   }, []);
   return (
     <div className={styles.board}>
@@ -22,20 +17,31 @@ export default function Board() {
         <div className={styles.row} key={i}>
           {new Array(8).fill(0).map((_, j) => {
             let p = pieces[i][j];
+            let c = "";
             if (p == ".") {
               p = "";
             } else if (p.match(/[A-Z]/)) {
               p = pw[p0.indexOf(p.toLowerCase())];
+              c = "w";
             } else {
               p = pb[p0.indexOf(p)];
+              c = "b";
             }
+            const square = `${"abcdefgh".charAt(j)}${8 - i}`;
             return (
               <div
                 className={[
                   styles.col,
                   (i + j) % 2 == 0 ? styles.w : styles.b,
+                  p && chess.turn() == c && styles.pointer,
+                  highlighted.includes(square) && styles.highlighted,
                 ].join(" ")}
                 key={`${i}, ${j}`}
+                onClick={() => {
+                  // @ts-ignore
+                  const mvs = chess.moves({ square, verbose: true }) as Move[];
+                  setHighlighted(mvs.map(({ to }) => to));
+                }}
               >
                 {p}
               </div>
--- utils/chess-utils.ts
+++ utils/chess-utils.ts
@@ -1,3 +1,13 @@
+import { Chess } from "chess.js";
+
 export const p0 = "rnbqkp";
 export const pb = "♜♞♝♛♚♟";
 export const pw = "♖♘♗♕♔♙";
+export const chess = new Chess();
+
+export const getBoard = () =>
+  chess
+    .ascii()
+    .split("\n")
+    .slice(1, 9)
+    .map((rank) => rank.slice(5, 27).split("  "));
